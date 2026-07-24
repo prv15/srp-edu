1926eeafc487ac0c4aed921ac4b9a47fc220eeae
prv15 <support@techpearlsolutions.com>
2026-07-24 19:23:54 +0530

    Point frontend to production API

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -3,10 +3,6 @@
  * Change only this URL when the API is hosted at a different location.
  * Do not place database credentials or other secrets in this browser file.
  */
-const isLocalDevelopment = ["localhost", "127.0.0.1"].includes(window.location.hostname);
-
 window.TPS_CONFIG = Object.freeze({
-    API_URL: isLocalDevelopment
-        ? "http://localhost:8000/api/v1"
-        : "https://thetechservices.in/srp-edu/api/v1",
+    API_URL: "https://thetechservices.in/srp-edu/api/v1",
 });
